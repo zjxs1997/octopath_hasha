6b03d12183f59a8558576e3ae9f9e12a15f14e7a
zjxs1997 <1500012780@pku.edu.cn>
2021-08-07 12:29:48 +0800

    追加半日课？

--- daily/半日课.js
+++ daily/半日课.js
@@ -174,6 +174,151 @@ function fight_colab() {
 
 }
 
+function origin2relic() {
+    common.click_world_map();
+    click(1326, 995)
+    sleep(1400)
+    // 到这里去
+    click(1870, 905)
+    sleep(1300)
+    // fast travel的确认
+    click(1432, 695)
+    while (!common.checkStanding()) sleep(1000);
+}
+
+function relic2flame() {
+    common.click_world_map();
+    click(1028, 182)
+    sleep(1400)
+    // 到这里去
+    click(1870, 905)
+    sleep(1300)
+    // fast travel的确认
+    click(1432, 695)
+    while (!common.checkStanding()) sleep(1000);
+    click(1233, 231)
+    sleep(1000);
+    while (!common.checkStanding()) sleep(1000);
+    click(1317, 440)
+    sleep(3500)
+    // 瞎鸡儿点
+    click(1463, 751)
+    sleep(1100)
+    // 确认宿屋，20块钱
+    click(1491, 716)
+    sleep(6000)
+    // 恢复后的确认
+    click(1236, 699)
+    sleep(1600)
+}
+
+// 蜥蜴人
+function fight_66_1() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.all_boost()
+    common.move(1, 2, false)
+    common.move(3, 4, false)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 2
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+}
+
+// 青蛙
+function fight_66_3() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.move(2, 1, true)
+    common.all_boost()
+    common.move(1, 3, false)
+    common.move(3, 4, false)
+    common.move(4, 2, true)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 2
+    common.all_boost()
+    common.start_attack(1000)
+
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+
+}
+
+function fight_66_tower() {
+
+    // 进入篝火
+    move_upward()
+    while (!common.checkStanding()) sleep(1000);
+    click(1224, 182)
+    sleep(1000)
+    click(934, 710)
+    while (!common.checkStanding()) sleep(1000);
+
+    // 去frost地区
+    click(1619, 225)
+    sleep(5500)
+    click(1513, 600)
+    while (!common.checkStanding()) sleep(1000);
+
+    // 蜥蜴人
+    common.click_little_map()
+    click(1058, 388)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_1()
+
+
+    // 去右边flatland
+    common.click_little_map()
+    click(1477, 681)
+    while (!common.checkStanding()) sleep(1000);
+
+    click(1450, 419)
+    sleep(800)
+    click(1544, 610)
+    while (!common.checkStanding()) sleep(1000);
+
+
+    // 青蛙
+    common.click_little_map()
+    click(1033, 422)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_3()
+}
+
+function fight_66_wide() {
+    tower2flame()
+    flame2origin()
+
+    common.click_little_map()
+    click(980, 790)
+    go_or_run()
+    common.swipe_left()
+    fight_66_1()
+
+    origin2relic()
+
+    common.click_little_map()
+    click(893, 488)
+    go_or_run()
+    common.swipe_left()
+    fight_66_3()
+
+    relic2flame()
+    flame2tower()
+}
+
+
+
 
 // -------------------------------------------------------
 // --------------------- 银雪系列 end ---------------------
@@ -355,11 +500,13 @@ function bingjin_wide() {
 // -------------------------------------------------------
 
 // 流程：篝火2
+fight_66_tower()
+fight_66_wide()
+fight_colab();
+switch_team(2);
 
 bingjin();
 bingjin_wide();
 
-switch_team(-2);
-fight_colab();
 
 
