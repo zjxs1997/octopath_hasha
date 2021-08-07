a06d18e937b91ede9b2f506b826c3eb14b3b8410
zjxs1997 <1500012780@pku.edu.cn>
2021-08-07 12:24:13 +0800

    日课追加

--- daily/日课m.js
+++ daily/日课m.js
@@ -872,7 +872,7 @@ function shayan_wide() {
 
 
 // -------------------------------------------------------
-// ----------------------- 银雪系列 -----------------------
+// ----------------------- 66系列 -----------------------
 // -------------------------------------------------------
 
 
@@ -980,9 +980,304 @@ function fight_colab() {
     end_combat();
 }
 
+// fight boss
+function fight_tiger() {
+    // 进入篝火
+    move_upward()
+    while (!common.checkStanding()) sleep(1000);
+    click(1224, 182)
+    sleep(1000)
+    click(934, 710)
+    while (!common.checkStanding()) sleep(1000);
+
+    // 去frost地区
+    click(1619, 225)
+    sleep(5500)
+    click(1513, 600)
+    while (!common.checkStanding()) sleep(1000);
+
+
+    common.click_little_map()
+    click(1192, 245)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    while (!common.checkCombat()) sleep(1000);
+    sleep(1000);
+
+    // fight start
+    // turn 1
+    common.move(4, 2, false)
+    common.all_boost()
+    common.move(1, 3, false)
+    common.move(2, 2, false)
+    common.move(3, 2, false)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 2
+    common.move(4, 1, true)
+    common.all_boost()
+    common.move(1, 3, false)
+    common.move(2, 2, false)
+    common.move(3, 2, false)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 3
+    common.move(4, 3, true)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 4
+    common.move(4, 1, true)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 5
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombatEnd()) sleep(1000);
+
+    end_combat()
+}
+
+function origin2relic() {
+    common.click_world_map();
+    click(1326, 995)
+    sleep(1400)
+    // 到这里去
+    click(1870, 905)
+    sleep(1300)
+    // fast travel的确认
+    click(1432, 695)
+    while (!common.checkStanding()) sleep(1000);
+}
+
+function relic2flame() {
+    common.click_world_map();
+    click(1028, 182)
+    sleep(1400)
+    // 到这里去
+    click(1870, 905)
+    sleep(1300)
+    // fast travel的确认
+    click(1432, 695)
+    while (!common.checkStanding()) sleep(1000);
+    click(1233, 231)
+    sleep(1000);
+    while (!common.checkStanding()) sleep(1000);
+    click(1317, 440)
+    sleep(3500)
+    // 瞎鸡儿点
+    click(1463, 751)
+    sleep(1100)
+    // 确认宿屋，20块钱
+    click(1491, 716)
+    sleep(6000)
+    // 恢复后的确认
+    click(1236, 699)
+    sleep(1600)
+}
+
+// 蜥蜴人
+function fight_66_1() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.all_boost()
+    common.move(1, 2, false)
+    common.move(3, 4, false)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 2
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+}
+
+// 魔导机兵
+function fight_66_2() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.all_boost()
+    common.move(1, 2, false)
+    common.move(3, 4, true)
+    common.move(4, 2, true)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+
+    // turn 2
+    common.move(2, 4, false)
+    common.move(4, 4, false)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+
+    // turn 3
+    common.move(4, 4, true)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+
+}
+
+// 青蛙
+function fight_66_3() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.move(2, 1, true)
+    common.all_boost()
+    common.move(1, 3, false)
+    common.move(3, 4, false)
+    common.move(4, 2, true)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 2
+    common.all_boost()
+    common.start_attack(1000)
+
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+
+}
+
+function fight_66_4() {
+    while (!common.checkCombat()) sleep(1000);
+
+    // turn 1
+    common.all_boost()
+    common.move(1, 3, false)
+    common.move(3, 4, false)
+    common.move(4, 2, true)
+    common.start_attack(1000)
+    while (!common.checkCombat()) sleep(1000);
+
+
+    // turn 2
+    common.move(2, 3, true)
+    common.move(4, 4, true)
+    common.start_attack(1000)
+
+    while (!common.checkCombat()) sleep(1000);
+
+
+    // turn 3
+    common.move(2, 2, false)
+    common.move(3, 2, false)
+    common.all_boost()
+    common.start_attack(1000)
+
+    while (!common.checkCombatEnd()) sleep(1000);
+    end_combat();
+
+}
+
+function fight_66_tower() {
+
+    // 进入篝火
+    move_upward()
+    while (!common.checkStanding()) sleep(1000);
+    click(1224, 182)
+    sleep(1000)
+    click(934, 710)
+    while (!common.checkStanding()) sleep(1000);
+
+    // 去frost地区
+    click(1619, 225)
+    sleep(5500)
+    click(1513, 600)
+    while (!common.checkStanding()) sleep(1000);
+
+    // 蜥蜴人
+    common.click_little_map()
+    click(1058, 388)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_1()
+
+
+    // 魔导机兵
+    common.click_little_map()
+    click(1273, 380)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_2()
+
+
+    // 去右边flatland
+    common.click_little_map()
+    click(1477, 681)
+    while (!common.checkStanding()) sleep(1000);
+
+    click(1450, 419)
+    sleep(800)
+    click(1544, 610)
+    while (!common.checkStanding()) sleep(1000);
+
+
+    // 青蛙
+    common.click_little_map()
+    click(1033, 422)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_3()
+
+
+    // 双头蛇
+    common.click_little_map()
+    click(1325, 432)
+    while (!common.checkStanding()) sleep(1000);
+    move_upward();
+    fight_66_4()
+}
+
+function fight_66_wide() {
+    tower2flame()
+    flame2origin()
+
+    common.click_little_map()
+    click(980, 790)
+    go_or_run()
+    common.swipe_left()
+    fight_66_1()
+
+    common.click_little_map()
+    click(973, 589)
+    go_or_run()
+    common.swipe_left()
+    fight_66_2()
+
+
+    origin2relic()
+
+    common.click_little_map()
+    click(893, 488)
+    go_or_run()
+    common.swipe_left()
+    fight_66_3()
+
+    common.click_little_map()
+    click(1454, 657)
+    go_or_run()
+    common.swipe_left()
+    fight_66_4()
+
+    relic2flame()
+    flame2tower()
+}
+
 
 // -------------------------------------------------------
-// --------------------- 银雪系列 end ---------------------
+// --------------------- 66系列 end ---------------------
 // -------------------------------------------------------
 
 
@@ -1458,22 +1753,33 @@ function bingjin_wide() {
 tower2flame();
 flame2tower();
 
+// 64
 switch_team(-1);
 shayan();
 // shayan_wide();
 tower2flame();
 flame2tower();
 
+// 66
 switch_team(2)
+fight_66_tower()
+fight_66_wide()
 fight_colab();
 tower2flame()
 flame2tower()
 
-switch_team(2);
+// 66 boss
+switch_team(1);
+fight_tiger()
+tower2flame()
+flame2tower()
+
+// 63
+switch_team(1);
 bingjin();
 bingjin_wide();
 
-
+// 65
 switch_team(2);
 shanhu();
 // shanhu_wide();
