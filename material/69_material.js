5b4286ad397e8f890e7b35477c123947b40b0729
zjxs1997 <1500012780@pku.edu.cn>
2021-11-11 21:32:40 +0800

    安排69的素材脚本

---
+++ material/69_material.js
@@ -0,0 +1,140 @@
+// 新地图刷素材脚本
+
+// 开局需要在海
+// 角色：1号塞拉斯，3号技能全雷
+// 其他工具人拉满即可
+
+var battle_times = 0;
+
+auto()
+
+var common = require("lib.js");
+
+//截屏权限请求
+if (!requestScreenCapture(true)) {
+    toast("请求截图失败");
+    exit();
+}
+
+function hole2town() {
+    common.click_world_map()
+    click(1771, 498)
+    sleep(1400)
+    // 到这里去
+    click(1870, 905)
+    sleep(1300)
+    // fast travel的确认
+    click(1432, 695)
+    sleep(8500)
+
+    // 进入旅店
+    click(1238, 327)
+    sleep(3600)
+    // 旅店柜台
+    click(1295, 393)
+    sleep(3500)
+    // 瞎鸡儿点
+    click(1463, 751)
+    sleep(1100)
+    // 确认宿屋，20块钱
+    click(1491, 716)
+    sleep(6000)
+    // 恢复后的确认
+    click(1236, 699)
+    sleep(1600)
+}
+
+function town2hole() {
+    common.click_world_map()
+    click(703, 571)
+    sleep(1200)
+    // 到这里去
+    click(1870, 905)
+    sleep(1200)
+    // fast travel的确认
+    click(1432, 695)
+    sleep(8500)
+}
+
+function playerMove(f) {
+    common.click_little_map();
+    if (f) {
+        click(1031, 401)
+    } else {
+        click(1630, 483)
+    }
+    sleep(500)
+}
+
+
+var total_battle_times = 0;
+var flag = true;
+var i = 0;
+
+function checker() {
+    while (1) {
+        if (common.checkCombat()) return 1;
+        if (common.checkCombatEnd()) return 0;
+        sleep(1000)
+    }
+}
+
+while (1) {
+    if (battle_times >= 12) {
+        // 重置各种状态
+        common.click_little_map()
+        click(2085, 59)
+        sleep(1000)
+        hole2town();
+        town2hole();
+        battle_times = 0;
+        flag = true;
+        toastLog('xxx')
+    }
+
+    if (common.checkStanding()) {
+        playerMove(flag);
+        flag = !flag;
+    }
+
+    i = 0;
+    while (i < 3 && battle_times < 12) {
+        sleep(2500)
+        if (common.checkCombat()) {
+            battle_times++;
+            total_battle_times++;
+            sleep(2000)
+
+            common.move(1, 4, false)
+            common.all_boost();
+            click(2020, 929)
+            sleep(1000)
+
+            var res = checker()
+            if (res == 1) {
+                log('尼玛，是猫')
+                click(1211, 957)
+                sleep(6000)
+            } else {
+                // 瞎鸡儿点
+                click(1463, 751)
+                sleep(2100)
+                // 瞎鸡儿点
+                click(1463, 751)
+                sleep(3100)
+                // 瞎鸡儿点
+                click(1463, 751)
+                sleep(5600)
+            }
+
+            log("战斗了" + total_battle_times + '次')
+
+            if (battle_times >= 12)
+                break;
+
+        } else {
+            i++;
+        }
+    }
+}
+
