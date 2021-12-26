fd4db77b3c66eb1faf118e6e731ef08db8162d44
xusheng <zjxs97@pku.edu.cn>
2021-12-26 14:28:29 +0800

    增加拉哥周回

---
+++ arena/largo.js
@@ -0,0 +1,339 @@
+// jjc药师半自动周回脚本，可能翻车
+// 开局保证在追忆之书，jjc事件最下方
+// 记得开战斗command记忆选项
+
+auto();
+
+//截屏权限请求
+if (!requestScreenCapture(true)) {
+    toast("请求截图失败");
+    exit();
+}
+
+var common = require("lib.js");
+
+counter = 0;
+fail_counter = 0;
+
+function handleDeath() {
+    if (common.checkBackDeath()) {
+        // devicePixelRatio.vibrate(1000)
+        log('有角色挂了，重开！')
+        click(1198, 933);
+        sleep(800);
+        click(1483, 693);
+    } else
+        return false;
+    while (!common.checkCloseWin()) sleep(1000);
+    fail_counter++;
+    click(1240, 584)
+    sleep(1000)
+    return true;
+}
+
+function click_down() {
+    click(825, 584)
+    sleep(800)
+}
+
+function click_up() {
+    click(1038, 456)
+    sleep(800)
+}
+
+function click_left() {
+    click(551, 556)
+    sleep(800)
+}
+
+var host = images.read("/storage/emulated/0/checkPic/host.jpg")
+function checkArenaEnd() {
+    var img = null;
+    while (!img) {
+        img = captureScreen();
+    }
+    var key = findImage(img, host, {
+        region: [910, 220, 100, 100]
+    })
+    img.recycle();
+    if (key)
+        return 1;
+    else
+        return 0;
+}
+
+
+function checker() {
+    while (1) {
+        if (common.checkArenaCombat()) return 0;
+        if (checkArenaEnd()) return 1;
+    }
+}
+
+function win() {
+    press(1850, 507, 2000)
+    sleep(8000)
+    click(1299, 771)
+    sleep(1000)
+
+    toastLog("周回了" + counter + "次，翻车" + fail_counter + "次")
+}
+
+function run() {
+    fail_counter++
+    click(1198, 933);
+    sleep(800);
+    click(1483, 693);
+    while (!common.checkCloseWin()) sleep(1000);
+    click(1240, 584)
+    sleep(1000)
+}
+
+
+
+
+while (1) {
+    // 読む
+    click(1920, 259)
+    sleep(800)
+    // 确认
+    click(1485, 884)
+    sleep(5000)
+    press(1250, 507, 2000)
+
+    while (!common.checkArenaCombat()) sleep(1000);
+
+    // turn 1
+    click_down()
+    common.move(4, 5, false)
+    common.all_boost()
+    common.fight(1, 1, true, 3)
+    common.move(2, 3, false)
+    common.move(3, 2, false)
+
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+
+
+    // turn 2
+    click_left()
+    common.all_boost()
+    common.move(1, 2, false)
+    common.move(4, 2, true)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 3
+    common.move(3, 2, true)
+    common.move(4, 3, false)
+    common.all_boost()
+    common.move(1, 4, false)
+    common.move(2, 3, false)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 4
+    common.move(1, 3, false)
+    common.all_boost()
+    common.move(2, 3, false)
+    common.move(3, 4, true)
+    common.move(4, 5, false)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 5
+    common.move(1, 5, true)
+    common.move(3, 2, true)
+    common.all_boost()
+    common.move(2, 3, false)
+    common.fight(4, 3, true, 3)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 6
+    common.move(1, 4, true)
+    common.move(3, 3, true)
+    common.move(4, 3, true)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 7
+    common.move(1, 0, true)
+    common.move(2, 2, false)
+    common.move(3, 2, false)
+    common.move(4, 0, true)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 8
+    common.move(3, 0, false)
+    common.move(4, 5, true)
+    common.all_boost()
+    common.move(2, 3, false)
+    common.fight(1, 1, true, 3)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 9
+    common.move(3, 2, true)
+    common.all_boost()
+    common.move(1, 4, false)
+    common.move(2, 3, false)
+    common.move(4, 5, false)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+    // turn 10
+    common.move(1, 5, true)
+    common.all_boost()
+    common.fight(2, 1, true, 4, 3)
+    common.move(4, 5, true)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 11
+    // 4th character, target down
+    click(2041, 721)
+    sleep(800)
+    click(2016, 931)
+    sleep(800)
+    click_down()
+    click(1609, 284)
+    sleep(800)
+
+    // 3rd character, target up
+    click(2041, 531)
+    sleep(800)
+    click(2016, 931)
+    sleep(800)
+    click_up()
+    click(1537, 409)
+    sleep(800)
+
+    common.all_boost()
+    common.move(2, 3, true)
+    common.move(1, 4, true)
+
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 12
+    common.move(4, 1, true)
+    common.move(3, 4, true)
+    common.all_boost()
+    common.move(1, 4, false)
+    common.move(2, 3, false)
+
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 13
+    common.move(1, 2, false)
+    common.move(2, 4, false)
+    common.move(3, 3, false)
+    common.move(4, 4, true)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 14
+    common.move(1, 4, false)
+    common.move(3, 4, false)
+    common.move(4, 1, true)
+    common.all_boost()
+    common.fight(2, 3, false, 3)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 15
+    common.all_switch()
+    common.move(2, 3, false)
+    common.move(4, 5, false)
+    common.all_boost()
+    common.move(1, 4, false)
+    common.move(3, 4, false)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 16
+    common.move(1, 5, false)
+    common.move(4, 3, false)
+    common.all_boost()
+    common.move(2, 3, true)
+    common.move(3, 2, false)
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 17
+    common.move(1, 0, false)
+    common.move(4, 0, true)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+
+    // turn 18
+    common.move(1, 4, true)
+    common.move(3, 0, false)
+    common.move(4, 0, true)
+    common.all_boost()
+    common.start_attack(1000)
+    while (!common.checkArenaCombat()) sleep(1000);
+    if (handleDeath()) continue;
+
+    // turn 19
+    common.move(2, 2, false)
+    common.move(3, 2, false)
+    common.move(4, 5, false)
+    common.all_boost()
+    common.start_attack(1000)
+    var res = checker();
+
+    if (res == 1) {
+        // win
+    } else {
+        run()
+    }
+
+    counter++;
+    win();
+
+    // 最后是检测结束时候的勲章＆閉じる
+    // 勋章确认
+    while (!common.checkCloseWin()) sleep(1000);
+    click(1240, 584)
+    sleep(1000)
+
+    log("周回了" + counter + "次！")
+}
+
